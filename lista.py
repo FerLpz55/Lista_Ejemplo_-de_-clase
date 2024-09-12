947f4a0394f32a7ed3db8912ca165ca46862cae0
FerLpz55 <rodriguezlopezfernando26@gmail.com>
2024-09-12 13:47:18 -0600

    Lista1

---
+++ lista.py
@@ -0,0 +1,12 @@
+lista = ["cheetos", 50, "coca", 80, "pepsi", 100, "suavitel", 500, "sabritas", 30, "boing", 300, "tortillinas", 40]
+
+lista.append("boing")
+lista.append(16)
+
+lista.append("tortillinas")
+lista.append(32)
+
+print(lista)
+
+for elemento in lista:
+    print(elemento)
