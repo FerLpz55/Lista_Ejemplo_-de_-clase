a2b0b8e793cc108dd8d77206bf56dd3ed1c646d0
FerLpz55 <rodriguezlopezfernando26@gmail.com>
2024-09-12 14:33:01 -0600

    cambio final

--- lista.py
+++ lista.py
@@ -1,12 +1,46 @@
-lista = ["cheetos", 50, "coca", 80, "pepsi", 100, "suavitel", 500, "sabritas", 30, "boing", 300, "tortillinas", 40]
 
-lista.append("boing")
-lista.append(16)
+#Fernando rodríguez lópez
+#236w0341
+#Estructura de datos 303A
+#12/09/2024
 
-lista.append("tortillinas")
-lista.append(32)
+#ejemplo de listas
 
-print(lista)
 
-for elemento in lista:
+
+lista1 = ["cheetos", 50, "coca", 80, "pepsi", 100, "suavitel", 500, "sabritas", 30, "boing", 300, "tortillinas", 40]
+lista2 = ["whirpool", 1050, "LG thinkq", 8000, "apple vision pro", 16500, "consola switch", 8500],
+lista3 = ["resident evil", 1000, "mario 64", 1000, "karlii", 700, "fifa25", 1000],
+
+
+lista1.append("boing")
+lista1.append(16)
+lista1.append("tortillinas")
+lista1.append(32)
+
+lista2.append("LG")
+lista2.append(2)
+lista2.append("Switch")
+lista2.append(5)
+
+lista3.append("fifa25")
+lista3.append(5)
+lista3.append("karlii")
+lista3.append(3)
+
+
+print("Impresión de la lista uno:")
+for elemento in lista1:
     print(elemento)
+print()  
+
+
+print("Impresión de la lista dos:")
+for elemento in lista2:
+    print(elemento)
+print() 
+
+print("Impresión de la lista tres:")
+for elemento in lista3:
+    print(elemento)
+print()  
